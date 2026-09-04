a6df2667cb50e5df0922cfc6759f7d0732adcfaf
Gmodery <gmodery@verizon.net>
2026-09-04 17:46:21 -0400

    Polymorphism & comments

---
+++ Polymorphism/Polymorphism.java
@@ -0,0 +1,53 @@
+package Polymorphism;
+
+
+class Dog implements Speaker {
+
+    public void speak() {
+        System.out.println("Woof!");
+    }
+
+    public void announce(String str) {
+        System.out.println(str);
+    }
+    
+}
+
+class Philosopher implements Speaker {
+
+    public void speak() {
+        System.out.println("I think, therefore I am");
+    }
+
+    public void announce(String str) {
+        System.out.println(str);
+    }
+
+    public void pontificate() {
+        System.out.println("One must think Sisyuphus happy");
+    }
+
+}
+
+
+public class Polymorphism {
+
+    public static void main(String[] args) {
+        // Can point to any implementer of the Speaker class (Dog or Philosopher)
+        Speaker guest;
+
+        // Depending on which class guest is initialized as, a different speak will be called
+        guest = new Philosopher();
+        guest.speak();
+        
+        guest = new Dog();
+        guest.speak();
+
+
+        // Invocations are limited to those defined in the interface
+        // A compiler error is thrown when trying to use those outside of it for the general Speaker interface without casting
+        Speaker special = new Philosopher();
+        // special.pontificate(); // Compiler error on this line
+        ((Philosopher)special).pontificate();
+    }
+}
---
+++ Polymorphism/Speaker.java
@@ -0,0 +1,8 @@
+package Polymorphism;
+
+// Creates an interface called Speaker
+// Remember that this defines what methods MUST be defined for all implementing classes
+public interface Speaker {
+    public void speak();
+    public void announce(String str);
+}
